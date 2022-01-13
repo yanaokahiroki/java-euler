5ae1793805752fa22d576057cf099201804a0ee1
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2022-01-13 22:16:29 +0900

    :wrench:  gitignore更新

--- src/euler/p004_最大の回文積.java
+++ src/euler/p004_最大の回文積.java
@@ -1,14 +1,13 @@
 package euler;
 
 /**
- * 「最大の回文積」
- * 左右どちらから読んでも同じ値になる数を回文数という. 2桁の数の積で表される回文数のうち, 最大のものは 9009 = 91 × 99 である.
+ * 「最大の回文積」 左右どちらから読んでも同じ値になる数を回文数という. 2桁の数の積で表される回文数のうち, 最大のものは 9009 = 91 × 99 である.
  *
  * <p>では, 3桁の数の積で表される回文数の最大値を求めよ.
  */
 public class p004_最大の回文積 {
-  public static void main(String[] args){
-    System.out.println(largestPalindrome(999,999));
+  public static void main(String[] args) {
+    System.out.println(largestPalindrome(999, 999));
   }
 
   /**
@@ -17,10 +16,10 @@ public class p004_最大の回文積 {
    * @param number 数
    * @return reversed反転させた数字
    */
-  public static long reverse(long number){
+  public static long reverse(long number) {
     long reversed = 0;
     long reduced;
-    while (number > 0){
+    while (number > 0) {
       // reducedにnumberの下1桁を代入
       reduced = number % 10;
       // reversedを10倍したものにreducedを足してreversedに代入
@@ -33,38 +32,25 @@ public class p004_最大の回文積 {
   }
 
   /**
-   * 回文かどうか判定
-   * reverseメソッドにnumberを入れて反転させる、
-   * もしそれが元のnumberと同じ値なら回文になるのでtrueを返す
+   * 回文かどうか判定 reverseメソッドにnumberを入れて反転させる、 もしそれが元のnumberと同じ値なら回文になるのでtrueを返す
    *
    * @return true
    */
-  public static boolean isPalindrome(long number){
+  public static boolean isPalindrome(long number) {
     return reverse(number) == number;
   }
 
-  public static long largestPalindrome(long a, long b){
+  public static long largestPalindrome(long a, long b) {
     long result;
     long largest = 0;
-    for (long x = a; x > 99; x--){
-      for (long y = b; y > 99; y--){
+    for (long x = a; x > 99; x--) {
+      for (long y = b; y > 99; y--) {
         result = y * x;
-        if (isPalindrome(result)){
+        if (isPalindrome(result)) {
           largest = Math.max(largest, result);
         }
       }
     }
     return largest;
   }
-
-
 }
-
-
-
-
-
-
-
-
-
